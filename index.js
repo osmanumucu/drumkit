53350a0e1a2f49b97a91e84574b9d256e72a4f63
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-05-16 11:35:27 +0200

    139 done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        this.style.color = "white"
+
+    });
 }
 
-function playSound() {
-    alert("Hi");
-}
 
+//var audio = new Audio('sounds/tom-1.mp3');
+//audio.play();  
